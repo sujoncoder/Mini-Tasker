3753af52140af29fc6fdd522a667f5bd6b5a0ad7
iamsujonsheikh <sujonsheikh.in@gmail.com>
2024-03-02 14:17:42 +0600

    project init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
