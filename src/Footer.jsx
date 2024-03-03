ad0257a85f0ec958258d3d91b929fd03d303d76a
iamsujonsheikh <sujonsheikh.in@gmail.com>
2024-03-03 14:56:13 +0600

    done

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="py-6 md:py-8">
       <div className="container mx-auto">
         <p className="text-center text-xs text-[#6A6A6A] lg:text-sm">
-          Copyright ©2024 | All rights reserved by Learn with Sumit
+          Copyright ©2024 | All rights reserved by Sheikh Sujon 👨‍💻
         </p>
       </div>
     </footer>
--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -15,8 +15,8 @@ const HeroSection = () => {
             />
           </div>
           <div>
-            <h1 className="mb-1.5 text-[56px] font-bold leading-none text-[#F5BF42] lg:text-[73px]">
-              Tasker
+            <h1 className="mb-1.5 text-[56px] font-bold leading-none text-blue-200 lg:text-[73px]">
+              Task-Tool
             </h1>
             <p className="text-lg my-2 opacity-60">
               Effortlessly Organize, Prioritize, and Conquer Tasks with Tasker -
